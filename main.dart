bbe333b93217e70a6ba4abb28326d6d6ee3a806c
bella1243 <abelbekanagot@gmail.com>
2023-11-24 13:27:12 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,8 +1,8 @@
 void main() {
-  var myList = [1, 2, 3, 5];
-  print(myList);
-  print(myList[0]);
-  myList[0] = 42;
+  var firstList = [1, 2, 3, 5];
+  print(firstList);
+  print(firstList[0]);
+  firstList[0] = 42;
   var secondList = [];
   print(secondList);
   secondList.add(1);
@@ -13,4 +13,11 @@ void main() {
     3,
   ]);
   print(secondList);
+  var thridList = [1, 2, 3, 4, 5];
+  thridList.insert(0, 232);
+  print(thridList);
+  thridList.insertAll(3, [11, 31, 31]);
+  print(thridList);
+  thridList.removeAt(5);
+  print(thridList);
 }
