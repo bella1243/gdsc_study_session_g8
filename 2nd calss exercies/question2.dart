b1104ffdf57bb39833ac3ca07c618d98dd8c229a
bella1243 <abelbekanagot@gmail.com>
2023-11-27 09:37:31 +0300

    second class

--- 2nd calss exercies/question2.dart
+++ 2nd calss exercies/question2.dart
@@ -1,3 +1,10 @@
+//Write a Dart function that takes a list of numbers as input and
+// calculates the sum of all even numbers in the list. example
+
+//numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//answer = 30
+
 void sumOfEvenNubers(List<int> li) {
   int sum = 0;
   for (int l in li) {
