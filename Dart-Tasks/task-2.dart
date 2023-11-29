99a508d6b450011365d0b557cd93715400e84106
bella1243 <abelbekanagot@gmail.com>
2023-11-29 10:43:50 +0300

    task2

--- Dart-Tasks/task-2.dart
+++ Dart-Tasks/task-2.dart
@@ -1,42 +1,42 @@
-void findMaximum(List<int> numbers) {
+int findMaximum(List<int> numbers) {
   int max = numbers[0];
   for (int number in numbers) {
     if (number > max) {
       max = number;
     }
   }
-  print("Maximum = $max ");
+  return max;
 }
 
-void findMinimum(numbers) {
+int findMinimum(numbers) {
   int min = numbers[0];
   for (int number in numbers) {
     if (number < min) min = number;
   }
-  print("Minmum =$min");
+  return min;
 }
 
-void calculateSum(List<int> numbers) {
+int calculateSum(List<int> numbers) {
   int sum = 0;
   for (int number in numbers) {
     sum += number;
   }
-  print("Sum =$sum");
+  return sum;
 }
 
-void calculateAverage(numbers) {
-  int sum = 0;
-  for (int number in numbers) {
-    sum += number;
-  }
-  var avarage = sum / 2;
-  print("The avarage = $avarage");
+double calculateAverage(numbers) {
+  var avarage = calculateSum(numbers) / numbers.length;
+  return avarage;
 }
 
 void main() {
   final numbers = [1, 8, 3, 3, 4, 45, 5, 69, 10, 39, 91, 10];
-  findMaximum(numbers);
-  findMinimum(numbers);
-  calculateSum(numbers);
-  calculateAverage(numbers);
+  var q1 = findMaximum(numbers);
+  print("maximum = $q1");
+  var q2 = findMinimum(numbers);
+  print("Minimum = $q2");
+  var q3 = calculateSum(numbers);
+  print("sum = $q3");
+  var q4 = calculateAverage(numbers);
+  print("Average = $q4");
 }
